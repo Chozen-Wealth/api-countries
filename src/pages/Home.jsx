35ba307eee43d1abe6a625944d5b964169a99617
chozen <moustafa.eldehni@hotmail.com>
2025-06-05 12:19:58 +0200

    Ajout de la barre de recherche

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,15 +1,36 @@
+import { useState } from "react";
 import Card from "../components/Card";
 
 
 export default function Home({data, setData}) {
 
+    const [selectedRegion, setSelectedRegion] = useState("")
+
+    const handleChange = (e) => {
+        setSelectedRegion(e.target.value);
+    }
 
     
     return(
         <div className='Home'>
+            <div className="divFiltres">
+                <div className="divInput">
+                    <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#000000"><path d="M784-120 532-372q-30 24-69 38t-83 14q-109 0-184.5-75.5T120-580q0-109 75.5-184.5T380-840q109 0 184.5 75.5T640-580q0 44-14 83t-38 69l252 252-56 56ZM380-400q75 0 127.5-52.5T560-580q0-75-52.5-127.5T380-760q-75 0-127.5 52.5T200-580q0 75 52.5 127.5T380-400Z"/></svg>
+                    <input type="search" name="" id="" placeholder="Search for a country..."/>
+                </div>
+                <select name="" id="" value={selectedRegion} onChange={handleChange}>
+                    <option value="" disabled hidden>Filter by Region</option>
+                    <option value="">All</option>
+                    <option value="Africa">Africa</option>
+                    <option value="America">America</option>
+                    <option value="Asia">Asia</option>
+                    <option value="Europe">Europe</option>
+                    <option value="Oceania">Oceania</option>
+                </select>
+            </div>
             <div className="allPays">
                 {data ? (data.map(element => (
-                    <Card drapeau={element.flags.png} pays={element.name.common} population={element.population} region={element.region} capital={element.capital} />
+                    <Card key={element.cca3} drapeau={element.flags.png} pays={element.name.common} population={element.population} region={element.region} capital={element.capital} />
                 ))) : "Loading..."}
             </div>
         </div>
