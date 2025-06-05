2b243aee13c398dbe25c5f7ad36b0994f8192e91
chozen <moustafa.eldehni@hotmail.com>
2025-06-05 10:16:54 +0200

    Structure de base

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,35 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Route, Routes } from 'react-router-dom'
+import axios from 'axios'
+import Home from './pages/Home'
+import Details from './pages/Details'
+import Layout from './pages/Layout'
 
 function App() {
+
+  const [data, setData] = useState([])
+
+
+
+
+
+  // Importation de l'API
+  useEffect(()=> {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then(response => setData(response.data))
+      .catch(error => console.log(error))
+  },[])
+
   return (
     <>
+      <Routes>
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path='/details/:id' element={<Details />} />
+        </Route>
+      </Routes>
     </>
   )
 }
---
+++ src/pages/Layout.jsx
@@ -0,0 +1,10 @@
+import Nav from "../components/Nav";
+
+
+export default function Layout() {
+    return(
+        <div className='Layout'>
+            <Nav />
+        </div>
+    )
+}
