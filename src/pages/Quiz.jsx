ecb661448287420e0a5cc196bef74d6af1a18c16
kottor <gardien243@gmail.com>
2025-06-06 15:50:38 +0200

    mise a jour

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,6 +1,11 @@
+import { useEffect } from "react"
+
 export default function Quiz({data}){
-    const listePays = data.map(el => el.name.common)
-    const region =  data.map(el=>el)
+    const listePays = data?.map(el => el.name.common)
+
+    useEffect(()=>{
+        const question = listePays
+    },[listePays])
 
     return(
         <section id="quiz">
