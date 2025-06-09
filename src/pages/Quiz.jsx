6f9ddd5ab559b90fc38c9990d6bd49447b1954d1
kottor <gardien243@gmail.com>
2025-06-09 10:30:50 +0200

    ça bug plus

--- src/pages/Quiz.jsx
+++ src/pages/Quiz.jsx
@@ -1,14 +1,20 @@
 import { useEffect, useState } from "react"
 
 export default function Quiz({data}){
-    const listePays = data?.map(el => ({nom: el.name.common,flag: el.flags}))
+    console.log(data&&data);
+    
+    const listePays = data?.map((el,index) => ({nom: el.name.common,flag: el.flags}))
     const [question, setQuestion] = useState([])
-    const [partie,setPartie] = useState({vie:10,score:0,victoire:0,defaite:0})
+    const [partie,setPartie] = useState({vie:10,score:0,victoire:0,defaite:0,question:0})
     const [reponse,setReponse] = useState()
 
     useEffect(() => {
         const start = () => {
+            setPartie({...partie,score:0,question:0})
             const listeQuestion = [];
+            if (listePays.length<=0){
+                return
+            }
             for (let i = 0; i < 5; i++) {
                     const random = [...listePays].sort(() => 0.5 - Math.random());
                     const candidat = random[0];
@@ -25,31 +31,37 @@ export default function Quiz({data}){
             };
 
         setQuestion(start());
-    }, [listePays]);
+    }, [data]);
     
 
     return(
         <section id="quiz">
-            <form onSubmit={handleSubmit}>
-                {question.map((q, i) => (
-                <div key={i}>
-                    <p>Quel est ce pays ?</p>
-                    <img src={q.bonneReponse.flags.svg} alt={`Drapeau de ${q.bonneReponse.flags.alt}`} />
+            {question?.length > 0 ? 
+            <>
+                <form>
+                    
                     <div>
-                        {q.reponses.map((rep, idx) => (
-                            <button value={rep} onClick={(e)=>(
-                                setReponse(e.target.value)
-                            )}>{rep}</button>
-                        ))}
+                        <p>Quel est ce pays ?</p>
+                        <img src={question[partie.question].bonneReponse.flag.svg} alt={question[partie.question].bonneReponse.flag.alt} />
+                        <div>
+                            {question[partie.question].reponses.map((rep, idx) => (
+                                <button key={idx} value={rep} onClick={(e)=>(
+                                    setReponse(e.target.value)
+                                )}>{rep}</button>
+                            ))}
+                        </div>
                     </div>
-                </div>
-                ))}
-
-            </form>
+                </form>
 
                 <p>
-                Ton score est {score} / {question.length}
+                Ton score est {partie.score} / {question.length}
+                tu as gagner {partie.victoire} / {partie.defaite}
                 </p>
+            </>
+            :
+            <p>chargement</p>
+            
+            }
             </section>
 
     )
